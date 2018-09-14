6bb1a68329d31aef15b35978a633f6f1c355fc88
Eugene Gavrilov <gavrilov.ea@gmail.com>
2018-09-14 23:57:49 +0500

    show form args

--- actions/tasks.go
+++ actions/tasks.go
@@ -8,6 +8,8 @@ import (
 
 // TasksCreate create new task in YouTrack.
 func TasksCreate(c buffalo.Context) error {
-	fmt.Printf("PARAMS: %v", c.Params())
+	// fmt.Printf("PARAMS: %v", c.Data())
+	fmt.Printf("Form: %v \n", c.Request().Form)
+
 	return c.Render(200, r.JSON(map[string]string{"status": "OK"}))
 }
