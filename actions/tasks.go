8d7fbd767336bcd52067d59b1973d3db31de1a60
Eugene Gavrilov <gavrilov.ea@gmail.com>
2018-09-14 23:48:30 +0500

    show params

--- actions/tasks.go
+++ actions/tasks.go
@@ -1,8 +1,13 @@
 package actions
 
-import "github.com/gobuffalo/buffalo"
+import (
+	"fmt"
+
+	"github.com/gobuffalo/buffalo"
+)
 
 // TasksCreate create new task in YouTrack.
 func TasksCreate(c buffalo.Context) error {
+	fmt.Printf("PARAMS: %v", c.Params())
 	return c.Render(200, r.JSON(map[string]string{"status": "OK"}))
 }
