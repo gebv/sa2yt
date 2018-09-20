50e68943b9699e6aa93001e7ad41b48507a3376c
Eugene Gavrilov <gavrilov.ea@gmail.com>
2018-09-21 01:06:22 +0500

    add answer for action

--- actions/slack_actions.go
+++ actions/slack_actions.go
@@ -11,19 +11,25 @@ import (
 func SlackActionsCreate(c buffalo.Context) error {
 	fmt.Printf("Form: %v \n", c.Request().Form)
 
-	return c.Render(200, r.JSON(lib.SlackResponse{
-		Text: "Task was created",
-		Attachments: []lib.SlackAttachment{
-			{
-				Fallback: fmt.Sprintf("View Task In YouTrack %s.", "urlToTask"),
-				Actions: []lib.SlackAction{
-					{
-						Type: "button",
-						Text: "View Task In YouTrack",
-						URL:  "urlToTask",
+	go func() {
+		responseURL := c.Request().Form.Get("response_url")
+
+		lib.SendAnswerToSlack(responseURL, &lib.SlackResponse{
+			Text: "Task was created",
+			Attachments: []lib.SlackAttachment{
+				{
+					Fallback: fmt.Sprintf("View Task In YouTrack %s.", "urlToTask"),
+					Actions: []lib.SlackAction{
+						{
+							Type: "button",
+							Text: "View Task In YouTrack",
+							URL:  "urlToTask",
+						},
 					},
 				},
 			},
-		},
-	}))
+		})
+	}()
+
+	return c.Render(200, r.Plain(""))
 }
--- lib/slack.go
+++ lib/slack.go
@@ -1,5 +1,12 @@
 package lib
 
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
 // SlackResponse - struct for response to Slack API
 type SlackResponse struct {
 	ResponseType string            `json:"response_type,omitempty"`
@@ -20,4 +27,31 @@ type SlackAction struct {
 	Text  string `json:"text"`
 	URL   string `json:"url"`
 	Style string `json:"style"`
+}
+
+// SendAnswerToSlack - send answer to slack chat
+func SendAnswerToSlack(url string, slackResponse *SlackResponse) error {
+	client := &http.Client{}
+
+	buffer := new(bytes.Buffer)
+	responseBody, err := json.Marshal(slackResponse)
+	if err != nil {
+		return err
+	}
+	buffer.WriteString(string(responseBody))
+
+	request, err := http.NewRequest("POST", url, buffer)
+	request.Header.Set("content-type", "application/json")
+	request.Header.Set("Accept", "application/json")
+
+	fmt.Printf("REQUEST TO SLACK --- %v \n", request)
+
+	response, err := client.Do(request)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("RESPPPPP", response)
+
+	return nil
 }
