5999c33e05b865c971a823b42cacbd80f432d77e
Eugene Gavrilov <gavrilov.ea@gmail.com>
2018-10-01 23:01:24 +0500

    build

--- actions/slack_actions.go
+++ actions/slack_actions.go
@@ -13,6 +13,7 @@ func SlackActionsCreate(c buffalo.Context) error {
 
 	go func() {
 		responseURL := c.Request().Form.Get("response_url")
+		fmt.Println("responseURL:  ", responseURL)
 
 		lib.SendAnswerToSlack(responseURL, &lib.SlackResponse{
 			Text: "Task was created",
@@ -29,6 +30,19 @@ func SlackActionsCreate(c buffalo.Context) error {
 				},
 			},
 		})
+
+		// map[payload:[{
+		// 		"type":"message_action",
+		// 		"token":"YmLuJsrTXziYJyioh17Kl7O7",
+		// 		"action_ts":"1538416708.046393",
+		// 		"team":{"id":"TCTMD8KE2","domain":"my-test-companytalk"},
+		// 		"user":{"id":"UCTMD8L30","name":"gavrilov.ea"},
+		// 		"channel":{"id":"CCUE4GAAC","name":"new-test-app"},
+		// 		"callback_id":"create_task",
+		// 		"trigger_id":"446610868420.435727291478.fd381ff42baaa14d5138081e0deabc8e",
+		// 		"message_ts":"1537472211.000200",
+		// 		"message":{"type":"message","user":"UCTMD8L30","text":"hi","client_msg_id":"7ca3327e-fde4-46de-8302-64d405ddcb39","ts":"1537472211.000200"},
+		// 		"response_url":"https:\/\/hooks.slack.com\/app\/TCTMD8KE2\/446693190419\/i3Nr0YMuy90XC8LucmUxQjaz"}]]
 	}()
 
 	return c.Render(200, r.Plain(""))
